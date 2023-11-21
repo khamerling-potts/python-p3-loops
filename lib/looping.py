94e81245dba9c30d208eec69040e3f3d37c67dc7
Kyra Hamerling-Potts <khamerling-potts@wustl.edu>
2023-11-21 13:11:26 -0500

    done

--- lib/looping.py
+++ lib/looping.py
@@ -2,12 +2,25 @@
 
 def happy_new_year():
     # code goes here!
-    pass
+    i = 10
+    while i > 0:
+        print(i)
+        i -= 1
+    print("Happy New Year!")
 
 def square_integers(int_list):
     # code goes here!
-    pass
+    sq_list = [num ** 2 for num in int_list]
+    return sq_list
 
 def fizzbuzz():
     # code goes here!
-    pass
+    for i in range(1, 101):
+        if not i % 15:
+            print("FizzBuzz")
+        elif not i % 5:
+            print ("Buzz")
+        elif not i % 3:
+            print("Fizz")
+        else:
+            print(i)
